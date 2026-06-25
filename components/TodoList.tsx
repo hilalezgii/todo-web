95a1787c4e6667855e679ea3d66605edf17e36a6
Hilal Ezgi Özlem <hilal_ezgi_2003@hotmail.com>
2026-06-25 14:29:08 +0300

    feat: add virtual list, zustand persist, and scrollbar styling

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,13 +1,36 @@
-import { memo } from 'react'
+import { memo, useRef } from 'react'
 import { TodoListProps } from '../types/todo'
 import TodoItem from './TodoItem'
+import { useVirtualizer } from '@tanstack/react-virtual'
 
 const TodoList = memo(({ todos, onDelete, onUpdate }: TodoListProps) => {
+  const parentRef = useRef<HTMLDivElement>(null)
+  const virtualizer = useVirtualizer({
+    count: todos.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 50,
+    overscan: 10,
+  })
   return (
-    <div>
-      {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} onDelete={onDelete} onUpdate={onUpdate} />
-      ))}
+    <div ref={parentRef} className="max-h-[400px] overflow-auto">
+      <div style={{ height: virtualizer.getTotalSize() }} className="relative">
+        {virtualizer.getVirtualItems().map((virtualItem) => (
+          <div
+            key={virtualItem.key}
+            style={{
+              position: 'absolute',
+              top: virtualItem.start,
+              width: '100%',
+            }}
+          >
+            <TodoItem
+              todo={todos[virtualItem.index]}
+              onDelete={onDelete}
+              onUpdate={onUpdate}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   )
 })
